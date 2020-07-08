aa7811bd0fa7e7d76243967722812b1d8751d2e2
GeorgeLIu <g5321247@gmail.com>
2020-07-08 21:36:18 +0800

    [feat] create CollectionViewCell and use it into SearchVC

--- GithubSearch/Search/SearchViewController.swift
+++ GithubSearch/Search/SearchViewController.swift
@@ -10,9 +10,56 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.delegate = self
+            collectionView.dataSource = self
+            collectionView.registerCollectionViewCell(
+                identifiers: [
+                    String(describing: UserCollectionViewCell.self)
+                ]
+            )
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 }
 
+// MARK: - CollectionView DataSource
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UserCollectionViewCell = collectionView.makeCell(indexPath: indexPath)
+        return cell
+    }
+}
+
+// MARK: - CollectionView Delegate
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sideSpacing: CGFloat = 20
+        let interitemSpacing: CGFloat = 20
+        let numberOfInterCell = 2
+        let width = ((UIApplication.shared.keyWindow?.screen.bounds.width)! - (sideSpacing + interitemSpacing * CGFloat(numberOfInterCell) - 1)) / CGFloat(numberOfInterCell)
+
+        return CGSize(width: width, height: width)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+}
---
+++ GithubSearch/Search/View/Cell/UserCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserCollectionViewCell.swift
+//  GithubSearch
+//
+//  Created by George Liu on 2020/7/8.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
