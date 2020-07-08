bb5b39e1652b8f7ee58481795837e8d1a0601956
GeorgeLIu <g5321247@gmail.com>
2020-07-08 22:22:48 +0800

    [feat] create Constant file

---
+++ GithubSearch/Unit/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  GithubSearch
+//
+//  Created by George Liu on 2020/7/8.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constant {
+    static let screenWidth: CGFloat  = UIScreen.main.bounds.size.width
+}
