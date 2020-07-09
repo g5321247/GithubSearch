b211b2948a47d0dc9ac1acecc4b4d0678eb20460
GeorgeLIu <g5321247@gmail.com>
2020-07-09 12:55:29 +0800

    [feat] create SearchRequest for searching GitHub users

---
+++ GithubSearch/Search/Request/SearchRequest.swift
@@ -0,0 +1,40 @@
+//
+//  SearchRequest.swift
+//  GithubSearch
+//
+//  Created by George Liu on 2020/7/9.
+//  Copyright © 2020 George Liu. All rights reserved.
+//
+
+import Foundation
+
+struct SearchRequest: Request {
+    typealias Response = SearchResult
+    let method: HTTPMethod = .get
+    let path: String = "search/users"
+    var parameter: [String : Any] {
+        return [
+            "q": parameters.queryKey
+        ]
+    }
+
+    let parameters: Parameters
+
+    struct Parameters {
+        let queryKey: String
+    }
+}
+
+struct SearchResult: Codable {
+    let itmes: [User]
+}
+
+struct User: Codable {
+    let name: String
+    let picURLStr: String
+
+    enum CodingKeys: String, CodingKey {
+        case name = "login"
+        case picURLStr = "avatar_url"
+    }
+}
